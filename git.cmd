9a1947b070a353e6150257640f8283367c50beaf
jrpgit <parmar.jr@gmail.com>
2021-01-14 16:42:14 -0800

    files

---
+++ git.cmd
@@ -0,0 +1,7 @@
+git status
+git init
+git add <file_name>
+git commit -m <message to be added>
+git remote add origin <repo remote location>
+git push -u origin master
+git pull --rebase origin main 
